eaa9f31150f8963154d113976f933b33ef775310
Uberlan Souza <uberlan-almeida@hotmail.com>
2026-01-27 21:05:19 -0300

    Primeiro commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { ContactForm } from "@/components/contact-form"
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-background py-8 px-4">
+      <div className="max-w-4xl mx-auto">
+        <ContactForm />
+      </div>
+    </main>
+  )
+}
